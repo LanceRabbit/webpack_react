1bf3344140b84f3db0477d6b09e70c76bd4f2618
LanceRabbit <lancerabbit@gmail.com>
2020-01-23 15:02:17 +0800

    react work without jsx

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("Hello Webpack~!!")
+import React from "react";
+import ReactDOM from "react-dom";
+
+let elem = React.createElement("h3", {}, "Hello React");
+ReactDOM.render(elem, document.querySelector("#root"));
